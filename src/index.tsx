a20713721f2b6f53970f147225dbf44e60202d80
Selvam Jayakumar <60879114+selvaramj@users.noreply.github.com>
2025-11-26 17:30:19 +0530

    Add FingerprintJS integration in App component
    Integrate FingerprintJS to generate and display a fingerprint hash.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,36 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+
+// import the fingerprintjs opensource library
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
+export default function App() {
+  // The fingerprint can be stored in the state or
+  // in the localstorage of the browser
+
+  const [fpHash, setFpHash] = React.useState('');
+
+  // create and set the fingerprint as soon as
+  // the component mounts
+  React.useEffect(() => {
+    const setFp = async () => {
+      const fp = await FingerprintJS.load();
+
+      const { visitorId } = await fp.get();
+
+      setFpHash(visitorId);
+    };
+
+    setFp();
+  }, []);
+  return (
+    <div>
+      <h1>This is the fingerprint hash</h1>
+      <h3>Hash: {fpHash}</h3>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
